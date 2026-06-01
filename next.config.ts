dda482f1c72c6deaaa9e62ffda5896c914d040f8
zee10zee <abedkhan.noori10@gmail.com>
2026-06-01 11:00:30 +0430

    skipwith.. deleted for paw making the app

--- next.config.ts
+++ next.config.ts
@@ -48,7 +48,6 @@ const nextConfig: NextConfig = {
 export default withPWA({
   dest: 'public',
   register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 })(nextConfig)
 
