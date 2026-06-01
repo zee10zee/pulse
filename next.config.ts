0674db0b9c04beb8560a88f29ae1c259e88f2ced
zee10zee <abedkhan.noori10@gmail.com>
2026-06-01 10:27:15 +0430

    update the next paw fork recommended version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 // next.config.js
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
