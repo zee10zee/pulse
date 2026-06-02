e25475ce32b94e37edbbc5b205365bc4f7751fe2
zee10zee <abedkhan.noori10@gmail.com>
2026-06-02 14:48:26 +0430

    fixing server exception for image upload of the posts by change nextconfig.js from ts

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-import withPWA from "@ducanh2912/next-pwa";
 
 
-const nextConfig: NextConfig = {
+const withPWA = require("@ducanh2912/next-pwa").default;
+
+
+const nextConfig = {
    experimental: {
     proxyClientMaxBodySize: '50mb', 
   },
